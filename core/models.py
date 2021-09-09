ea3457163a43983ea8d5b981bcbde02da50c6ecb
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-09-09 23:07:33 +0100

    updating backend

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ import os
 from django.db.models.signals import pre_delete
 from django.dispatch.dispatcher import receiver
 
-
+from django.core.validators import RegexValidator
 
 
 def settings_image(instance, filename):
@@ -19,7 +19,16 @@ class Settings(models.Model):
     class Meta:
         verbose_name_plural = "Settings"
 
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in a valid format.")
+
     main_background_image = models.ImageField(upload_to=settings_image)
+    admin_name = models.CharField(max_length=50, unique=True, null=True)
+    admin_email = models.EmailField(max_length=255, unique=True, null=True)
+    admin_phone_number = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True, null=True)
+    admin_address = models.CharField(
+        max_length=255, unique=True, blank=True, null=True)
 
     def __str__(self):
         return 'My Settings'
@@ -34,11 +43,11 @@ class Project(models.Model):
         return self.name
 
 
-
 def _delete_file(path):
     # Deletes file from filesystem.
     if os.path.isfile(path):
         os.remove(path)
+
 
 @receiver(pre_delete, sender=Settings)
 def delete_img_pre_delete_post(sender, instance, *args, **kwargs):
