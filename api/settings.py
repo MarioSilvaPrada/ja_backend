cdc851ffbe05feff4dd7cfa0fa9d4e739f06f15d
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-09-18 23:08:26 +0100

    added

--- api/settings.py
+++ api/settings.py
@@ -28,6 +28,8 @@ DEBUG = int(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
+CORS_ORIGIN_WHITELIST = ('http://localhost:9000',)
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,16 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #3rd party
+    # 3rd party
     'rest_framework',
+    'corsheaders',
 
-    #Local
+    # Local
     'core'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
