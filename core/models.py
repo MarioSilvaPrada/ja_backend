2a519c97ae778567ae2f2cd3a535779ce5c93ed4
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-10-02 13:09:39 +0100

    deleting unecessary code

--- core/models.py
+++ core/models.py
@@ -91,3 +91,16 @@ class Partners(models.Model):
     url = models.URLField(
         max_length=155, verbose_name='Site da empresa', blank=True)
 
+
+
+def delete_from_s3(bucket, model, aws_secret, aws_key):
+    try:
+        s3 = boto3.client(
+            "s3", aws_access_key_id=aws_key, aws_secret_access_key=aws_secret
+        )
+        s3.delete_object(Bucket=bucket, Key=model)
+        return True
+    except Exception as ex:
+        print(str(ex))
+        return False
+
