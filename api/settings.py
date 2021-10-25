64831c012fd6c867d4c1b48f4a2d32543a7e7408
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-10-25 22:41:01 +0100

    fixing

--- api/settings.py
+++ api/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-print('aquii', os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
